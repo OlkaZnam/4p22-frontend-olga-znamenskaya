060d36170aca8f101ef54d69af721e374465d138
OlkaZnam <lunar.raccoon@yandex.ru>
2022-10-27 23:23:34 +0900

    add homework_8

--- homework_8/app.js
+++ homework_8/app.js
@@ -13,16 +13,16 @@ button.addEventListener ('click', (event) => {
   event.preventDefault();
 
   
-  if (isNaN(num1) == true || isNaN(num2) === true || (num1.trim() == '' && num2.trim() == '')) {
+  if (isNaN(num1) === true || isNaN(num2) === true || (num1.trim() === '' && num2.trim() === '')) {
     console.log('Некорректный ввод чисел');
     return calcResult.innerText = 'Некорректный ввод чисел';
-  } else if (num1.trim() == '') {
+  } else if (num1.trim() === '') {
     console.log('Певрое число не указано');
     return calcResult.innerText = 'Первое число не указано';
-  } else if (num2.trim() == '') {
+  } else if (num2.trim() === '') {
     console.log('Второе число не указано');
     return calcResult.innerText = 'Второе число не указано';
-   } else if (operator.trim() == '') {
+   } else if (operator.trim() === '') {
     console.log('Не введён знак');
     return calcResult.innerText = 'Не введён знак'; 
   }; 
